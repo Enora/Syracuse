97d69506e533dafec88c641483bce1e1b90aafc5
Enora <enoragab@wanadoo.fr>
2019-01-26 11:06:43 +0100

    Update conjSyracuse.py

--- conjSyracuse.py
+++ conjSyracuse.py
@@ -5,3 +5,22 @@ Created on Fri Jan 25 17:31:46 2019
 @author: user
 """
 
+# Permet decalculer les éléments d'une conjecture de Syracuse
+
+n = int(input('Entrez nu nombre'))
+
+
+def syracuse(n):
+    syrac = list["",n]
+    while (n>1):
+        if (n%2 == 0):
+            n = n/2
+        else:
+            n = (n*3)+1
+        syrac.append(n)
+    
+    dico = ['liste']=syrac
+    dico = ['longueur syrac'] = len(syrac)
+    return dico
+
+syracuse(n)
